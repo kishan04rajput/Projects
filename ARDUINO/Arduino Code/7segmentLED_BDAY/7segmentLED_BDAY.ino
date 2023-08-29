a50f5cf664dd9077a4d122e0c897daa757404155
kishan04rajput <44379153+kishan04rajput@users.noreply.github.com>
2023-08-29 11:58:17 +0530

    Create 7segmentLED_BDAY.ino

---
+++ ARDUINO/Arduino Code/7segmentLED_BDAY/7segmentLED_BDAY.ino
@@ -0,0 +1,298 @@
+int k = 500;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, OUTPUT); //A
+  pinMode(3, OUTPUT); //B
+  pinMode(4, OUTPUT); //C
+  pinMode(5, OUTPUT); //D
+  pinMode(6, OUTPUT); //E
+  pinMode(7, OUTPUT); //F
+  pinMode(8, OUTPUT); //G
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  H();
+  delay(k);
+  A();
+  delay(k);
+  P();
+  delay(k);
+  P();
+  delay(k);
+  Y();
+  delay(k);
+  B();
+  delay(k);
+  I();
+  delay(k);
+  R();
+  delay(k);
+  T();
+  delay(k);
+  H();
+  delay(k);
+  D();
+  delay(k);
+  A();
+  delay(k);
+  Y();
+  delay(k);
+  B();
+  delay(k);
+  H();
+  delay(k);
+  A();
+  delay(k);
+  L();
+  delay(k);
+  U();
+  delay(k);
+  BLANK();
+  delay(k);
+}
+void A() {
+  digitalWrite(2, 1);  //A
+  digitalWrite(3, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 0);
+  digitalWrite(6, 1);
+  digitalWrite(7, 1);
+  digitalWrite(8, 1);
+}
+void B() {
+  digitalWrite(2, 1);  //B
+  digitalWrite(3, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 1);
+  digitalWrite(6, 1);
+  digitalWrite(7, 1);
+  digitalWrite(8, 1);
+}
+void C() {
+  digitalWrite(2, 1);  //C
+  digitalWrite(3, 0);
+  digitalWrite(4, 0);
+  digitalWrite(5, 1);
+  digitalWrite(6, 1);
+  digitalWrite(7, 1);
+  digitalWrite(8, 0);
+}
+void D() {
+  digitalWrite(2, 1);  //D
+  digitalWrite(3, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 1);
+  digitalWrite(6, 1);
+  digitalWrite(7, 1);
+  digitalWrite(8, 0);
+}
+void E() {
+  digitalWrite(2, 1);  //E
+  digitalWrite(3, 0);
+  digitalWrite(4, 0);
+  digitalWrite(5, 1);
+  digitalWrite(6, 1);
+  digitalWrite(7, 1);
+  digitalWrite(8, 1);
+}
+void f() {
+  digitalWrite(2, 1);  //F
+  digitalWrite(3, 0);
+  digitalWrite(4, 0);
+  digitalWrite(5, 0);
+  digitalWrite(6, 1);
+  digitalWrite(7, 1);
+  digitalWrite(8, 1);
+}
+void G() {
+  digitalWrite(2, 1);  //G
+  digitalWrite(3, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 1);
+  digitalWrite(6, 0);
+  digitalWrite(7, 1);
+  digitalWrite(8, 1);
+}
+void H() {
+  digitalWrite(2, 0);  //H
+  digitalWrite(3, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 0);
+  digitalWrite(6, 1);
+  digitalWrite(7, 1);
+  digitalWrite(8, 1);
+}
+void I() {
+  digitalWrite(2, 0);  //I
+  digitalWrite(3, 0);
+  digitalWrite(4, 0);
+  digitalWrite(5, 0);
+  digitalWrite(6, 1);
+  digitalWrite(7, 1);
+  digitalWrite(8, 0);
+}
+void J() {
+  digitalWrite(2, 0);  //J
+  digitalWrite(3, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 1);
+  digitalWrite(6, 1);
+  digitalWrite(7, 0);
+  digitalWrite(8, 0);
+}
+void K() {
+  digitalWrite(2, 0);  //K
+  digitalWrite(3, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 0);
+  digitalWrite(6, 1);
+  digitalWrite(7, 1);
+  digitalWrite(8, 1);
+}
+void L() {
+  digitalWrite(2, 0);  //L
+  digitalWrite(3, 0);
+  digitalWrite(4, 0);
+  digitalWrite(5, 1);
+  digitalWrite(6, 1);
+  digitalWrite(7, 1);
+  digitalWrite(8, 0);
+}
+void M() {
+  digitalWrite(2, 1);  //M
+  digitalWrite(3, 0);
+  digitalWrite(4, 1);
+  digitalWrite(5, 0);
+  digitalWrite(6, 1);
+  digitalWrite(7, 0);
+  digitalWrite(8, 0);
+}
+void N() {
+  digitalWrite(2, 0);  //N
+  digitalWrite(3, 0);
+  digitalWrite(4, 1);
+  digitalWrite(5, 0);
+  digitalWrite(6, 1);
+  digitalWrite(7, 0);
+  digitalWrite(8, 1);
+}
+void O() {
+  digitalWrite(2, 1);  //O
+  digitalWrite(3, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 1);
+  digitalWrite(6, 1);
+  digitalWrite(7, 1);
+  digitalWrite(8, 0);
+}
+void P() {
+  digitalWrite(2, 1);  //P
+  digitalWrite(3, 1);
+  digitalWrite(4, 0);
+  digitalWrite(5, 0);
+  digitalWrite(6, 1);
+  digitalWrite(7, 1);
+  digitalWrite(8, 1);
+}
+void Q() {
+  digitalWrite(2, 1);  //Q
+  digitalWrite(3, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 0);
+  digitalWrite(6, 0);
+  digitalWrite(7, 1);
+  digitalWrite(8, 1);
+}
+void R() {
+  digitalWrite(2, 0);  //R
+  digitalWrite(3, 0);
+  digitalWrite(4, 0);
+  digitalWrite(5, 0);
+  digitalWrite(6, 1);
+  digitalWrite(7, 0);
+  digitalWrite(8, 1);
+}
+void S() {
+  digitalWrite(2, 1);  //S
+  digitalWrite(3, 0);
+  digitalWrite(4, 1);
+  digitalWrite(5, 1);
+  digitalWrite(6, 0);
+  digitalWrite(7, 1);
+  digitalWrite(8, 1);
+}
+void T() {
+  digitalWrite(2, 0);  //T
+  digitalWrite(3, 0);
+  digitalWrite(4, 0);
+  digitalWrite(5, 1);
+  digitalWrite(6, 1);
+  digitalWrite(7, 1);
+  digitalWrite(8, 1);
+}
+void U() {
+  digitalWrite(2, 0);  //U
+  digitalWrite(3, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 1);
+  digitalWrite(6, 1);
+  digitalWrite(7, 1);
+  digitalWrite(8, 0);
+}
+void V() {
+  digitalWrite(2, 0);  //H
+  digitalWrite(3, 0);
+  digitalWrite(4, 1);
+  digitalWrite(5, 1);
+  digitalWrite(6, 1);
+  digitalWrite(7, 0);
+  digitalWrite(8, 0);
+}
+void W() {
+  digitalWrite(2, 0);  //W
+  digitalWrite(3, 1);
+  digitalWrite(4, 0);
+  digitalWrite(5, 1);
+  digitalWrite(6, 0);
+  digitalWrite(7, 1);
+  digitalWrite(8, 0);
+}
+void X() {
+  digitalWrite(2, 0);  //X
+  digitalWrite(3, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 0);
+  digitalWrite(6, 1);
+  digitalWrite(7, 1);
+  digitalWrite(8, 1);
+}
+void Y() {
+  digitalWrite(2, 0);  //Y
+  digitalWrite(3, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 1);
+  digitalWrite(6, 0);
+  digitalWrite(7, 1);
+  digitalWrite(8, 1);
+}
+void Z() {
+  digitalWrite(2, 1);  //Z
+  digitalWrite(3, 1);
+  digitalWrite(4, 0);
+  digitalWrite(5, 1);
+  digitalWrite(6, 1);
+  digitalWrite(7, 0);
+  digitalWrite(8, 1);
+}
+void BLANK() {
+  digitalWrite(2, 0);  //BLANK
+  digitalWrite(3, 0);
+  digitalWrite(4, 0);
+  digitalWrite(5, 0);
+  digitalWrite(6, 0);
+  digitalWrite(7, 0);
+  digitalWrite(8, 0);
+}
